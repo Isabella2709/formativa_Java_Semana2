1939996cfa45e1c736d0a61cc1790887ec9c1dff
Isabella Ribas <isabellards@outlook.com>
2024-08-29 18:23:59 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        InterfaceUsuario obj = new InterfaceUsuario();
+
+        obj.pedirTaxaJuros();
+
+        Financiamento financiamento1 = new Financiamento(250.000,48, 7.8);
+
+
+    }
+}
